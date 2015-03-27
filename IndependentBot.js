f8d3045c2a55706a27a326521e21a0d5d7b3541b
mysticSilence <mysticalchickenwing@gmail.com>
2015-03-27 04:15:03 -0500

    Create IndependentBot.js

---
+++ IndependentBot.js
@@ -0,0 +1,12 @@
+//Plug.dj Independent V1
+//basically just a simple test :3
+
+API.on(API.CHAT_COMMAND, getCommand);
+function getCommand(cmd){
+	cmd = toLowerCase(cmd);
+	var args = cmd.split(" ");
+
+	if(args[0] == "test"){
+		console.log("testing");
+	}
+};
