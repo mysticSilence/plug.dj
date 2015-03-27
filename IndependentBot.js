24d990829ac8c2463cd10bc3bc03ea7d3a56e36b
mysticSilence <mysticalchickenwing@gmail.com>
2015-03-27 04:17:54 -0500

    Update IndependentBot.js

--- IndependentBot.js
+++ IndependentBot.js
@@ -3,8 +3,8 @@
 
 API.on(API.CHAT_COMMAND, getCommand);
 function getCommand(cmd){
-	cmd = toLowerCase(cmd);
-	var args = cmd.split(" ");
+	var cmnd = toLowerCase(cmd);
+	var args = cmnd.split(" ");
 
 	if(args[0] == "test"){
 		console.log("testing");
