de0932cef7219893d94c1d28dfafa26ba58b0311
mysticSilence <mysticalchickenwing@gmail.com>
2015-03-31 10:23:54 -0500

    Update mysticBot.js

--- mysticBot.js
+++ mysticBot.js
@@ -1,5 +1,4 @@
 //Plug.dj mysticBot V0.01
-//basically just a simple test :3
 
 //bot constants
 var version = 1; 
@@ -12,6 +11,13 @@ var manager = API.ROLE.MANAGER; //3
 var bouncer = API.ROLE.BOUNCER; //2
 var dj = API.ROLE.DJ; //1
 var guest = API.ROLE.NONE; //0
+
+var time = [
+	var hr = 0;
+	var min = 0;
+	var sec = 0;
+	var str = "00:00:00";
+]
 
 //users
 var users = API.getUsers();
@@ -31,13 +37,16 @@ function getChat(chat){ //chat command parser
 		if(getRole(sender, 5) || getRole(sender, 4)){ //if host or cohost
 		};
 		if(getRole(sender, 3)){ //if at least manager
+		};
+		if(getRole(sender, 2)){ //if at least bouncer
 			if(chat.message == "!skip"){
 				API.moderateForceSkip()
 			};
 		};
-		if(getRole(sender, 2)){ //if at least bouncer
-		};
 		if(getRole(sender, 1)){ //if at least dj
+			if(args[0].toLowerCase() == "!kill"){
+				API.sendChat("@" + sender.username + ": user " + args[1] + " has been killed.");
+			}
 		};
 		if(getRole(sender, 0)){ //if guest
 			if(chat.message == "!ver"){
@@ -45,7 +54,7 @@ function getChat(chat){ //chat command parser
 			};
 
 			//multi-arg
-			if(args[0] == "!getRole" || args[0] == "!gr"){
+			if(args[0].toLowerCase() == "!getrole" || args[0] == "!gr"){
 				role = "guest";
 				args[1] = args[1] + " " + args[2];
 				args.pop();
@@ -56,7 +65,7 @@ function getChat(chat){ //chat command parser
 								role = "Guest";
 								break;
 							case 1:
-								role = "DJ";
+								role = " DJ";
 								break;
 							case 2:
 								role = "Bouncer";
@@ -85,5 +94,22 @@ function parseEntry(user){
 	API.sendChat("Welcome to the community, " + user.username + ". I currently have little functionality, but am under development.");
 	users.push(user);
 };
+
+function timeUpdate(){
+	sec++;
+	if(sec == 59){
+		sec = 0;
+		min++;
+		if(min == 59){
+			hr++;
+			if(hr == 23){
+				hr = 0;
+			};
+		}
+	}
+}
+
 API.on(API.CHAT, getChat); //chat handler
 API.on(API.USER_JOIN, parseEntry); //user join parser
+
+setInterval(timeUpdate, 1000);
